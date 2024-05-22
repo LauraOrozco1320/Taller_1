158a011899b4fe85b68ec8dfcb621eed470a3636
LauraOrozco1320 <orozcolauracamila@gmail.com>
2024-05-22 11:35:24 -0500

    POINT START

---
+++ database/migrations/2024_05_22_141350_create_modulos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modulos', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+
+            //atributos foraneos 
+            $table->unsignedBigInteger('alumno_id')->nullable();
+
+            //referencia
+            $table->foreign('alumno_id')
+                ->references('id')
+                ->on('alumnos')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modulos');
+    }
+};
